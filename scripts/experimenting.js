42276db22e4bcec2a28d029e0d563055e81dc7a0
efullmetal <eddieslozano@gmail.com>
2023-09-29 16:24:28 -0400

    Add all files and the faulty javascript that is here

---
+++ scripts/experimenting.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <div class="nav-links">
+                <a href="\pages\home.html">Home</a>
+                <a href="\pages\projects.html">Projects</a>
+                <a href="\pages\about.html">About</a>
+                <a href="\pages\contact.html">Contact</a>
+            </div>
+        </nav>
+
+    )
+}

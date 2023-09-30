f9a9336d3bceb0176a1ee531bec1b6478426b258
efullmetal <eddieslozano@gmail.com>
2023-09-30 01:12:59 -0400

    this is last updated version today

--- scripts/experimenting.js
+++ scripts/experimenting.js
@@ -1,13 +1,12 @@
-export default function Navbar() {
-    return (
-        <nav>
-            <div class="nav-links">
-                <a href="\pages\home.html">Home</a>
-                <a href="\pages\projects.html">Projects</a>
-                <a href="\pages\about.html">About</a>
-                <a href="\pages\contact.html">Contact</a>
-            </div>
-        </nav>
-
-    )
+function createNavigationBar() {
+    const navBar = document.createElement("nav");
+    navBar.innerHTML = `
+        <div class="nav-links">
+            <a href="#home">Home</a>
+            <a href="#projects">Projects</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+        </div>
+    `;
+    return navBar;
 }

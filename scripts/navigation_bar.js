5a124ce0f839c0f2524c51f163f6a7aedaddb856
Eduardo Lozano <eddie@Eduardos-MBP.lan>
2024-01-10 15:57:28 -0500

    Just pushed whatever edits we had most recent

--- scripts/navigation_bar.js
+++ scripts/navigation_bar.js
@@ -1,39 +1,11 @@
-
-// All items we'd like to add
-var navItems = [
-    {href: './index.html', text: 'Home'},
-    {href: './projects.html', text: 'Projects'},
-    {href: './about.html', text: 'About'},
-    {href: './contact.html', text: 'Contact'}
-];                    
-
-// create our div to put the links in
-const navDiv = document.createElement("div");
-navDiv.classList.add("nav-links");
-
-//get the nav element from the HTML
-const navBar = document.getElementById("navBar");
-
-
-//insert our div after nav
-navBar.insertBefore(navDiv, navBar.children[0]);
-//add all the navItems to our navDiv
-
-var navItem, navLink;
-
-for (var i = 0; i < navItems.length; i++) {
-    navLink = document.createElement("a");
-
-    // Set properties on anchor
-    navLink.href = navItems[i].href;
-    navLink.innerHTML = navItems[i].text;
-
-    // Add anchor to list item, and list item to list
-    navDiv.appendChild(navLink);
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Eduardo's Site</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="\create">Projects</a>
+            </div>
+        </nav>
+    ) 
 }
-
-// Testing code
-/*
-console.log(navDiv)
-console.log(navBar)
-*/

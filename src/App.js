088927d43d1873809c0ff17e84823cc838b9e46c
eduardoloz <eddieslozano@gmail.com>
2024-01-12 00:07:47 -0500

    Fixed routing issue when rendering home pages. Note to self: old documetation uses switches and this has been replaced with <Routes />. Additionally, some funky stuff happens with path component but look into that later

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import { Component } from 'react'
+import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
+
 import Navbar from './components/Navbar/Navbar.js'
 import Home from './pages/Home/index.js'
-import Projects from './pages/projects.js'
+import Projects from './pages/Projects.js'
 import Contact from './pages/contact.js'
 
+
+//Note routes is outdated
+
 function App() {
-  let Component
-  switch(window.location.pathname){
-    case "/":
-      Component = Home
-      break
-    case "/contact":
-      Component = Contact
-      break
-    case '/projects':
-      Component = Projects
-      break
-  }
   return (
-    <>
+    <Router>
       <Navbar />
-      <Component />
-    </>
+        <Routes>
+          <Route path='/' element={<Home /> }/>
+          <Route path='/Projects' element={<Projects />}/>
+          <Route exact path='/Contact' element={<Contact />}/>
+        </Routes>
+    </Router>
   );
 }
 

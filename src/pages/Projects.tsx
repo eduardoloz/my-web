0af93039609e9a1aa83833e5e955b43879d22915
eduardoloz <eddieslozano@gmail.com>
2024-12-28 15:50:44 -0500

    Fixed some rendering but there is an issue in the first two projects that load

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,28 +5,28 @@ import projects from '../data/projects.json'; // Ensure the import path is corre
 const Projects: React.FC = () => {
   return (
     <div className="pt-20">
-      <div className="container flex flex-col items-center lg:items-start gap-12 mt-14">
-        <h1 className="text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6 lg:pl-10">
-          My Projects
-        </h1>
-        <p className="text-lg md:text-xl lg:text-2xl text-center lg:text-left mb-6 lg:pl-10">
-          Here are some of the projects I have worked on:
-        </p>
-        <div className="flex flex-col space-y-4 lg:space-y-6">
-          {projects.map((project) => (
-            <Project
-              key={project.id}
-              id={project.id}
-              title={project.title}
-              author={project.author}
-              description={project.description}
-              technologies={project.technologies}
-              image={project.image}
-            />
-          ))}
-        </div>
-      </div>
+  <div className="container mx-auto flex flex-col gap-12 mt-14 px-4">
+    <h1 className="text-3xl md:text-4 lg:text-5xl text-center lg:text-left mb-6">
+      My Projects
+    </h1>
+    <p className="text-lg md:text-xl lg:text-2xl text-center lg:text-left mb-6">
+      Here are some of the projects I have worked on:
+    </p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      {projects.map((project) => (
+        <Project
+          key={project.id}
+          id={project.id}
+          title={project.title}
+          author={project.author}
+          description={project.description}
+          technologies={project.technologies}
+        />
+      ))}
     </div>
+  </div>
+</div>
+
   );
 };
 
